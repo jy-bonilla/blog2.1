a0ad6a703f04974d78d1cdf4f99af2f3ce6a91e9
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2023-10-22 20:45:56 -0400

    added slug component page.jsx and single page css

---
+++ src/app/[slug]/page.jsx
@@ -0,0 +1,49 @@
+import style from './singlePage.module.css'
+import Menu from "@/components/menu/Menu"
+import Image from 'next/image'
+
+const Singlepage = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <div className={textContainer}>
+                    <h1> Lorem ipsum dolor sit amet</h1>
+                    <div className={styles.user}>
+                        <div className={styles.userImageContainer}>
+                            <Image src="/p1.jpeg" alt="" fill className={styles.avatar} />
+                        </div>
+                        <div className={styles.userTextContainer}>
+                            <span className={styles.username}>Jon Doe</span>
+                            <span className={styles.date}>10.23.2023</span>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <div className={styles.post}>
+                    <div className={styles.description}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Voluptates quo iusto molestiae mollitia nam sapiente et
+                            quisquam aliquid illum distinctio, ullam earum,
+                            ex expedita fugit natus esse commodi perspiciatis. Cumque?
+                        </p>
+                        <h2>Header</h2>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Voluptates quo iusto molestiae mollitia nam sapiente et
+                            quisquam aliquid illum distinctio, ullam earum,
+                            ex expedita fugit natus esse commodi perspiciatis. Cumque?
+                        </p>
+                    </div>
+                </div>
+                <Menu />
+            </div>
+        </div>
+    )
+}
+
+export default Singlepage
