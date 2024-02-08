c55169302bf6170166a2989bafe18e80562efe72
jy-bonilla <91812771+jy-bonilla@users.noreply.github.com>
2024-02-07 20:47:37 -0500

    added write page along with reactquill library

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -4,16 +4,16 @@ import styles from "./writePage.module.css";
 import Image from "next/image";
 import { useState } from "react";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.buble.css"
+import "react-quill/dist/quill.bubble.css"
 
 const WritePage = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Title" className={styles.input}/>
       <div className={styles.editor}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
@@ -29,8 +29,14 @@ const WritePage = () => {
             </button>
           </div>
         )}
-        <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story..." />
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..." />
       </div>
+      <button className={styles.publish}>Publish</button>
     </div>
   )
 }
